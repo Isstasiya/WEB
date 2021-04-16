261a284593938b9e2ea85153afdd491aa6c90bd1
Isstasiya <stasiy.issayeva@gmail.com>
2021-04-16 21:27:20 +0600

    ~change

--- WEB_7/DW_2/DW_2.py
+++ WEB_7/DW_2/DW_2.py
@@ -28,13 +28,13 @@ def lget():
                   <body>
                     <center>
                       <h1>Пейзажи Марса</h1>
-                      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" keyboard=True interval=50 width=1dp>
+                      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                         <ol class="carousel-indicators">
                           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                         </ol>
-                        <div class="carousel-inner">
+                        <div class="carousel-inner" role="listbox" style="width:50%">
                           <div class="carousel-item active">
                             <img class="d-block w-100" src="{file1}" alt="First slide">
                           </div>
